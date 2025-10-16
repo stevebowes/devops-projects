f516046d91588449a142709ab3c71f63f6b5c208
stevebowes <steve.bowes@icloud.com>
2025-10-16 13:35:40 -0400

    security

--- 01-EKS-CLUSTER/terraform/secrets.tf
+++
@@ -1,4 +0,0 @@
-# This file is now empty as secrets have been moved to their respective project directories:
-# - Development/Production app secrets: 03-CICD-APP-DEPLOYMENT/terraform/secrets.tf
-# - Grafana admin password: 04-MONITORING-STACK/terraform/secrets.tf
-# - OpenSearch admin password: 05-LOGGING-STACK/terraform/secrets.tf
